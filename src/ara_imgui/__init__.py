709294f77d003d60cae6af53d1ea0d353b56594f
HanamileH <hanamileh@gmail.com>
2025-07-01 17:14:26 +0300

    Remove theme module from imports and exports

--- src/ara_imgui/__init__.py
+++ src/ara_imgui/__init__.py
@@ -1,6 +1,5 @@
 import imgui
 from .app import App, run
-from .theme import apply_theme
 from .window import Window
 
-__all__ = ['App', 'run', 'apply_theme', 'Window', 'imgui']
+__all__ = ['App', 'run', 'Window', 'imgui']
